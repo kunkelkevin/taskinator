202111f50d065b6ef53ff94f832cc679f57d72c5
Kevin Kunkel <Christopher.Kunkel@traviscountytx.gov>
2021-04-06 08:54:50 -0500

    load tasks from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -209,6 +209,50 @@ var saveTasks = function(){
   localStorage.setItem("tasks",JSON.stringify(tasks));
 };
 
+var loadTasks = function(){
+  // Get task items from local Storage
+  tasks = localStorage.getItem("tasks");
+  
+  if (!tasks){
+    tasks = [];
+    return false;
+  }
+  
+  tasks = JSON.parse(tasks);
+  for (i=0; i<tasks.length; i++){
+    tasks[i].id = taskIdCounter;
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.setAttribute("data-task-id",tasks[i].id);
+    
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + tasks[i].name + "</h3><span class='task-type'>" + tasks[i].type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    var taskActionsEl = createTaskActions(tasks[i].id);
+    listItemEl.appendChild(taskActionsEl);
+    console.log(tasks[i].status);
+    if (tasks[i].status === "to do"){
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+      tasksToDoEl.appendChild(listItemEl);
+    } else if (tasks[i].status === "in progress"){
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+      tasksInProgressEl.appendChild(listItemEl);
+    } else if (tasks[i].status === "completed"){
+      listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+      tasksCompletedEl.appendChild(listItemEl);
+    }
+
+    console.log(listItemEl);
+    taskIdCounter++;
+  }
+  // Convert tasks from the string format back into an array of objects
+
+  // Iterate through a tasks array and create task elements on the page from it
+
+}
+
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -217,3 +261,5 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 // for changing the status
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
